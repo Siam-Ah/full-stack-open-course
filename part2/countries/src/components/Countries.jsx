d1c8dec11a69f651655ae9ef4fc73d0c5eb2a5e8
Siam <sahm0036@student.monash.edu>
2024-08-15 14:13:23 +1000

    2.19 done

--- part2/countries/src/components/Countries.jsx
+++ part2/countries/src/components/Countries.jsx
@@ -1,30 +1,53 @@
+import { useState } from 'react'
+
 export default function Countries(props) {
+    const [selectedCountry, setSelectedCountry] = useState(null)
+
     const filteredCountries = props.countries.filter(
         country => country.name.common.toUpperCase().includes(props.countryName.toUpperCase())
     )
 
     return (
-        filteredCountries.length !== 0 ?
-            <div>
+        <div>
+            {filteredCountries.length !== 0 ? (
+              <div>
                 {
-                    filteredCountries.length > 10 ? "Too many matches, specify another filter" : 
-                        filteredCountries.length > 1 ?
-                            <div>
-                                {filteredCountries.map(country => <div>{country.name.common}</div>)}
-                            </div> :
-                                <div>
-                                    <h2>{filteredCountries[0].name.common}</h2>
-                                    capital {filteredCountries[0].capital[0]}
-                                    <br />area {filteredCountries[0].area}
-                                    <ul>
-                                        {
-                                            Object.values(filteredCountries[0].languages).map(language => <li>{language}</li>)
-                                        }
-                                    </ul>
-                                    <img src={filteredCountries[0].flags.png} alt={filteredCountries[0].alt}/>
-                                </div>
+                  filteredCountries.length > 10 ? (
+                    "Too many matches, specify another filter"
+                  ) : filteredCountries.length > 1 ? (
+                    <div>
+                      {filteredCountries.map((country) => (
+                        <div key={country.cca3}>
+                          {country.name.common}
+                          <button onClick={() => setSelectedCountry(country)}>show</button>
+                        </div>
+                      ))}
+                    </div>
+                  ) : (
+                    <CountryInfo country={filteredCountries[0]}/>
+                  )
                 }
-            </div> :
-            null
+              </div>
+            ) : null}
+
+            {selectedCountry && <CountryInfo country={selectedCountry} />}
+        </div>
+          
+    )
+}
+
+function CountryInfo(props) {
+    return (
+        <div>
+            <h2>{props.country.name.common}</h2>
+            <p>Capital: {props.country.capital}</p>
+            <p>Area: {props.country.area}</p>
+            <ul>
+            {Object.values(props.country.languages).map((language, index) => (
+                <li key={index}>{language}</li>
+            ))}
+            </ul>
+            <img src={props.country.flags.png} alt={props.country.name.common} />
+        </div>
     )
 }
